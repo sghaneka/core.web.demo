8d5ecf20f9ef203e1211e7dec84313a28b89e8b2
sghaneka <acura379>
2017-05-22 12:33:56 -0400

    updates

--- Core.Web.Demo/src/About/main.js
+++ Core.Web.Demo/src/About/main.js
@@ -3,9 +3,42 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+
+
+//ReactDOM.render(React.DOM.h1(null, "Hello World another way22!")
+//    , document.getElementById("app"));
+
 //ReactDOM.render(
 //    <h1>Hello, world!</h1>,
 //    document.getElementById('app')
 //);
 
-ReactDOM.render(React.DOM.h1(null, "Hello World another way22!"), document.getElementById("app"));
+// Demonstration of props
+var HelloWorld = React.createClass({
+    render: function () {
+        return (
+            <p>Hello, {this.props.greetTarget} world!</p>
+        );
+    }
+});
+
+// Demonstration of children and props
+var Buttonify = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <button type={this.props.behavior}>{this.props.children}</button>
+            </div>
+        );
+    }
+});
+
+
+ReactDOM.render(
+    <div>
+        <HelloWorld greetTarget="BatMan" />
+        <HelloWorld greetTarget="MegaMan" />
+        <Buttonify behavior="Submit">SEND DATA</Buttonify> 
+    </div>,
+    document.getElementById('app')
+);
