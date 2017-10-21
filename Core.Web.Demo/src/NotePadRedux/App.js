abbe6acda408359e515775d1df8b10bf422f8ed7
samgh <samgh@DESKTOP-V2LTOKR>
2017-10-21 13:41:39 -0400

    upds

--- Core.Web.Demo/src/NotePadRedux/App.js
+++ Core.Web.Demo/src/NotePadRedux/App.js
@@ -25,80 +25,12 @@ class App extends React.Component {
         }
     }
 
-    componentWillMount() {
-        firebase.initializeApp({
-            apiKey: "AIzaSyDEVVqhVWVzXXugjJ0-1znoTPGS_mg7mFM",
-            authDomain: "notepad-2a0dd.firebaseapp.com",
-            databaseURL: "https://notepad-2a0dd.firebaseio.com",
-            projectId: "notepad-2a0dd",
-            storageBucket: "",
-            messagingSenderId: "80985797530"
-        });
-
-        console.log('Firebase success...');
-
-        firebase.database().ref('/notes')
-            .on('value', snapshot => {
-                const fbstore = snapshot.val();
-
-                const store = _.map(fbstore, (value, id) => {
-                    return {
-                        id: id,
-                        title: value.title,
-                        details: value.details
-                    };
-                });
-
-                this.setState({
-                    notes: store
-                });
-                
-            })
-    }
-
-    deleteNote(id) {
-        firebase.database().ref(`/notes/${id}`).remove();
-    }
-
-    handleChange(event) {
-        const name = event.target.name;
-        const value = event.target.value;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    handleSubmit(event) {
-       // alert(`Your note ${this.state.currentTitle} has been added!!!`);
-        event.preventDefault();
-        const data = {
-            title: this.state.currentTitle,
-            details: this.state.currentDetails
-        };
-
-        firebase.database().ref('/notes').push(data, response =>
-            response);
-
-        this.setState({
-            currentTitle: '',
-            currentDetails: ''
-        });
-    }
-
-    
   render() {
     return (
       <div className="App">
             <Header name={this.state.name} />            
-            <Form
-                currentTitle={this.state.currentTitle}
-                currentDetails={this.state.currentDetails}
-                handleChange={this.handleChange.bind(this)}
-                handleSubmit={this.handleSubmit.bind(this)}
-            />
-            <Grid notes={this.state.notes} deleteNote={this.deleteNote.bind(this)}  />
-
+            <Form />
+            <Grid />
       </div>
     );
   }
---
+++ Core.Web.Demo/src/NotePadRedux/readme_npm_install.txt
@@ -0,0 +1,2 @@
+﻿npm install --save-dev react react-dom
+npm install --save-dev react-redux redux redux-immutable-state-invariant redux-thunk lodash firebase
