b2c3c576f59ac5b08a09a31f8fdf6cf1865260da
sam g <sghaneka@yahoo.com>
2017-11-06 08:29:59 -0500

    new items

---
+++ Core.Web.Demo/src/ReactDinnerMenusApp/Components/RecipieChooser.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+
+class RecipieChooser extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { value: 'chicken_noodle_soup' };
+    }
+
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>Select the Recipie:
+                    <select value={this.state.value} onChange={this.handleChange}>
+                    </select>
+                </label>
+            </form>
+            );
+    }
+}
+
+export default RecipieChooser;
